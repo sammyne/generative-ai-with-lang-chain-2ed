05fdd79405e24cd8b5e5569cf5bbb21fd03b3501
sammyne <xiangmin-li@qq.com>
2025-11-25 05:00:02 +0000

    feat(chapter08): 1st review

--- chapter08/main.py
+++
@@ -1,9 +0,0 @@
-from langchain.evaluation import load_evaluator
-
-conciseness_evaluator = load_evaluator(
-    "criteria", criteria="conciseness", llm=evaluation_llm
-)
-conciseness_result = conciseness_evaluator.evaluate_strings(
-    prediction=prediction_health, input=prompt_health
-)
-print("Conciseness evaluation result:", conciseness_result)
